f49def3c8a86943bef31e9ace5df1dbca8872b0d
devgl96 <georgelucas.dev@gmail.com>
2022-02-09 22:21:15 -0300

    Inserção do React Router Dom no App

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,36 @@
 import React from 'react';
+
+import axios from "axios";
+import { useEffect, useState } from "react";
+
 import './App.css';
+import { Header } from './components/Header';
+import {
+  BrowserRouter as Router,
+  Routes,
+  Route,
+  Link,
+} from 'react-router-dom';
+import { Listagem } from './pages/Listagem';
+import { Cadastro } from './pages/Cadastro';
+
+
 
 function App() {
   return (
-    <div className="App">
-      <h1>Hello World</h1>
-    </div>
+    <Router>
+      <Header />
+
+      <Routes>
+        <Route path="/" element={<Listagem />} />
+
+        <Route path="/cadastro" element={<Cadastro />} />
+      </Routes>
+    </Router>
   );
 }
 
 export default App;
+
+
+{/*  */ }
