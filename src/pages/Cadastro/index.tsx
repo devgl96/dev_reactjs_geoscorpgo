1c7f5b47256e8c6066aa075873d527fd05c53653
devgl96 <georgelucas.dev@gmail.com>
2022-02-09 22:20:14 -0300

    Página Cadastro versão 1.0

--- src/pages/Cadastro/index.tsx
+++ src/pages/Cadastro/index.tsx
@@ -1,5 +1,12 @@
 export function Cadastro() {
   return(
-    <h1>Cadastro</h1>
+    <>
+      <h1>Cadastro</h1>
+      <label>Título</label>
+      <input type="text" placeholder="Digite o título do produto" />
+      <label>Descrição</label>
+      <input type="text" placeholder="Digite o descrição do produto" />
+      <button>Salvar</button>
+    </>
   );
 }
