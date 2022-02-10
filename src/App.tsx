10626429d80fd3660df51227d82c3f8379fc9548
devgl96 <georgelucas.dev@gmail.com>
2022-02-10 02:00:49 -0300

    Últimas atualizações no código geral

--- src/App.tsx
+++ src/App.tsx
@@ -1,36 +1,29 @@
-import React from 'react';
-
-import axios from "axios";
-import { useEffect, useState } from "react";
-
-import './App.css';
 import { Header } from './components/Header';
 import {
   BrowserRouter as Router,
   Routes,
   Route,
-  Link,
 } from 'react-router-dom';
+
 import { Listagem } from './pages/Listagem';
 import { Cadastro } from './pages/Cadastro';
 
-
+import { ProductsProvider } from './hooks/useProducts';
 
 function App() {
   return (
-    <Router>
-      <Header />
+    <ProductsProvider>
+      <Router>
+        <Header />
 
-      <Routes>
-        <Route path="/" element={<Listagem />} />
+        <Routes>
+          <Route path="/" element={<Listagem />} />
 
-        <Route path="/cadastro" element={<Cadastro />} />
-      </Routes>
-    </Router>
+          <Route path="/cadastro" element={<Cadastro />} />
+        </Routes>
+      </Router>
+    </ProductsProvider>
   );
 }
 
 export default App;
-
-
-{/*  */ }
