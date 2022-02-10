4e80370c3646d0d1a387f669dd584ff170399731
devgl96 <georgelucas.dev@gmail.com>
2022-02-10 01:59:23 -0300

    Componente Product versão final

--- src/components/Product/index.tsx
+++ src/components/Product/index.tsx
@@ -1,13 +1,23 @@
+import { useProducts } from "../../hooks/useProducts";
 import { Container, Content } from "./styles";
 
-export function Product() {
+interface ProductProps {
+  id: number;
+  name: string;
+  description: string;
+  image_url: string;
+};
+
+export function Product({id, name, description, image_url}: ProductProps) {
+  const { deleteProduct } = useProducts();
+
   return (
     <Container>
-      <span>x</span>
+      <span onClick={() => deleteProduct(id)}>x</span>
       <Content>
-        <img src="https://source.unsplash.com/random" alt="Random Image" />
-        <h3>Title</h3>
-        <p>Description</p>
+        <img src={image_url} alt={name} />
+        <h3>{name}</h3>
+        <p>{description}</p>
       </Content>
     </Container>
   )
--- src/components/Product/styles.ts
+++ src/components/Product/styles.ts
@@ -1,24 +1,32 @@
 import styled from "styled-components";
 
 export const Container = styled.div`
+  margin: 0; 
+
   position: relative; 
 
   width: 250px;
   height: 320px;
 
-  background: #303030;
+  background: var(--third-color);
   border-radius: 5px;
 
   span {
     font-size: 1.5rem;
     font-family: sans-serif;
     font-weight: normal;
-    color: #FFF;
+    color: var(--primary-color);
     
     position: absolute;
 
     right: 0.5rem;
     top: 0;
+
+    cursor: pointer;
+
+    &:hover {
+      color: #FF0000;
+    }
   }
 `;
 
@@ -38,11 +46,28 @@ export const Content = styled.div`
   }
 
   h3 {
-    color: #FFF;
-    margin: 1.5rem 0 0;
-  }
+    color:  var(--primary-color);
+    margin: 1.5rem 0.5rem 0;
 
+    text-align: center;
+  }
+  
   p {
-    color: #FFF;
+    color:  var(--primary-color);
+    text-align: center;
+
+    font-size: 0.9rem;
+
+    margin: 0.5rem 1rem;
+
+    /* Código para criar reticências no final do texto */
+    overflow: hidden;
+    text-overflow: ellipsis;
+    display: -webkit-box;
+    line-height: 16px; 
+    max-height: 50px;     
+      
+    -webkit-line-clamp: 3; 
+    -webkit-box-orient: vertical;
   }
 `;
